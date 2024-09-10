3429e40c56a8990551c0ad254644db79c2f0f2b7
yaman3108 <133077812+yaman3108@users.noreply.github.com>
2024-09-10 15:33:45 +0530

    Update EXP15.2.cpp

--- EXP15.2.cpp
+++ EXP15.2.cpp
@@ -1,12 +1,11 @@
 #include<iostream>
 using namespace std;
 
-// Function to calculate factorial
 int add(int n) {
-    if (n <= 1) { // Terminating statement (Base Condition)
+    if (n <= 1) { 
         return 1;
     } else {
-        return (n+fact(n - 1)); // Recursion
+        return (n+fact(n - 1)); 
     }
 }
 
@@ -14,7 +13,7 @@ int main() {
     int X, n;
     cout << "Enter a number: "; 
     cin >> n;      
-    X = add(n); // Function calling
+    X = add(n);
     cout << "addition of all numbers = " << X << endl;
     return 0;
 }
